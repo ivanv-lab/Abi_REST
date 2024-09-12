a2e7ec6a2478935c1fb3c5920576c7793454c33d
ivanv-lab <ivanvozisov33@yandex.ru>
2024-09-12 11:35:31 +0300

    Сделать авторизацию

--- FileResponse.cs
+++ FileResponse.cs
@@ -2,6 +2,8 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Data;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
 
 namespace Abi_REST
 {
@@ -10,14 +12,12 @@ namespace Abi_REST
         public async Task<string> ReadFile()
         {
             string path = Path.Combine(Environment.CurrentDirectory, "Files/file.xls");
-
             var workbook = new Workbook(path);
             workbook.Save(Path.Combine(
                 Environment.CurrentDirectory, "Files/out.json"));
             string file = await File.ReadAllTextAsync(Path.Combine(
                 Environment.CurrentDirectory, "Files/out.json"));
             return file;
-            
         }
     }
 }
